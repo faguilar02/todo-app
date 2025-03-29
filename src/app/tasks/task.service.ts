76feb4679114d90e65a5b3143b5501038013a40a
faguilar02 <daguilarv1@upao.edu.pe>
2025-03-28 22:42:54 -0500

    bugs have been (task service)

--- src/app/tasks/task.service.ts
+++ src/app/tasks/task.service.ts
@@ -4,9 +4,26 @@ import { State } from './interfaces/state';
 
 @Injectable({ providedIn: 'root' })
 export class TasksService {
-  private _tasks = signal<Task[]>([]);
-  public tasks = computed(() => this._filteredTasks() ?? this._tasks());
-  private _filteredTasks = signal<Task[] | null>(null);
+  private _tasks = signal<Task[]>(this.loadFromLocalStorage());
+  private _searchText = signal<string>('');
+
+  public tasks = computed(() => {
+    let filteredTasks = this._tasks().filter((task) => {
+      const state = this._state();
+      if (state === 'Pending') return !task.completed;
+      if (state === 'Completed') return task.completed;
+      return true;
+    });
+
+    const searchText = this._searchText();
+    if (searchText) {
+      filteredTasks = filteredTasks.filter((task) =>
+        task.text.toLowerCase().includes(searchText)
+      );
+    }
+    return filteredTasks;
+  });
+
   private _state = signal<State>('All');
   public numberOfTasks = computed<number>(() => this._tasks().length);
   public completedTasks = computed<number>(
@@ -19,93 +36,38 @@ export class TasksService {
   public isDeleting = signal<boolean>(false);
 
   addTask(task: Task) {
-    // console.log(task)
     this._tasks.update((tasks) => [...tasks, { ...task }]);
     this.saveToLocalStorage();
-    this.clearFilter();
-    // console.log(this._tasks)
   }
 
   changeIsCompleted(id: number) {
     this._tasks.update((tasks) =>
       tasks.map((t) => (t.id === id ? { ...t, completed: !t.completed } : t))
     );
-
     this.saveToLocalStorage();
-
-    const filteredTasks = this._tasks().filter((task) => {
-      switch (this._state()) {
-        case 'Pending':
-          return task.completed === false;
-
-        case 'Completed':
-          return task.completed === true;
-
-        default:
-          return task;
-      }
-    });
-
-    this._filteredTasks.set(filteredTasks);
   }
 
   searchTaskByText(text: string) {
-    // console.log(this._state());
     const formatedText = text.toLowerCase().trim();
-    const filteredTasks = this._tasks().filter((task) => {
-      switch (this._state()) {
-        case 'Pending':
-          return task.text.includes(formatedText) && task.completed === false;
-
-        case 'Completed':
-          return task.text.includes(formatedText) && task.completed === true;
-
-        default:
-          return task.text.includes(formatedText);
-      }
-    });
-
-    this._filteredTasks.set(filteredTasks);
+    this._searchText.set(formatedText);
   }
 
   editValueTask(task: Task, text: string) {
-
     this._tasks.update((tasks) =>
       tasks.map((t) => (t.id === task.id ? { ...t, text: text } : t))
     );
-
-    // console.log(this._tasks());
 
     this.saveToLocalStorage();
   }
 
   searchByState(state: State) {
-    switch (state) {
-      case 'All':
-        this.clearFilter();
-        this._state.set('All');
-
-        break;
-      case 'Pending':
-        this._state.set('Pending');
-        this._filteredTasks.set(
-          this._tasks().filter((task) => !task.completed)
-        );
-        break;
-      case 'Completed':
-        this._state.set('Completed');
-        this._filteredTasks.set(this._tasks().filter((task) => task.completed));
-        break;
-    }
-  }
-
-  clearFilter() {
-    this._filteredTasks.set(null);
+    this._state.set(state);
   }
 
   loadFromLocalStorage() {
     const storedTasks = localStorage.getItem('tasks');
-    this._tasks.set(storedTasks ? JSON.parse(storedTasks) : []);
+
+    return storedTasks ? JSON.parse(storedTasks) : [];
   }
 
   saveToLocalStorage() {
--- src/app/tasks/task/task.component.ts
+++ src/app/tasks/task/task.component.ts
@@ -22,12 +22,11 @@ import { CustomFocusDirective } from '../../shared/directives/custom-focus.direc
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class TaskComponent {
-
-  public isDeleting = output<boolean>()
+  public isDeleting = output<boolean>();
   public task = input.required<Task>();
   private tasksService = inject(TasksService);
   public isEditing = signal<boolean>(false);
-  public taskToDelete = output<Task>()
+  public taskToDelete = output<Task>();
 
   changeToCompleted(id: number) {
     this.tasksService.changeIsCompleted(id);
@@ -40,23 +39,20 @@ export class TaskComponent {
 
   changeValueOfEditingByBlur() {
     setTimeout(() => {
-
       this.isEditing.set(false);
     }, 150);
   }
 
-  emitDeletingValue(task:Task){
-    this.isDeleting.emit(true)
-    this.taskToDelete.emit(task)
-
-
+  emitDeletingValue(task: Task) {
+    this.isDeleting.emit(true);
+    this.taskToDelete.emit(task);
   }
 
   deleteTask(id: number) {
     this.tasksService.deleteTask(id);
   }
 
-  editValueTask(task: Task, text: string ) {
+  editValueTask(task: Task, text: string) {
     // console.log('editandoo')
     // console.log({task:task, text:text})
     this.tasksService.editValueTask(task, text);
